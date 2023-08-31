ecfe1cf57b5f0e5b66d216147665c7f9b2617033
Peter Müller <peter@crycode.de>
2023-08-31 17:41:44 +0200

    Port from @crycode/eslint-config

---
+++ README.md
@@ -0,0 +1,56 @@
+# ESLint config
+
+This modules provides an ESLint configuration to be used with **plain JavaScript**
+according to my personal preferences.
+
+## Usage
+
+Add `eslint` and `@crycode/eslint-config-js` to your devDependencies:
+
+```shell
+npm install --save-dev eslint @crycode/eslint-config-js
+```
+
+Add a file named `.eslintrc.js` to your project root directory:
+
+```js
+module.exports = {
+  extends: [
+    '@crycode/eslint-config-js',
+  ],
+};
+```
+
+## Changelog
+
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ### **WORK IN PROGRESS**
+-->
+### 1.0.0 (2023-08-31)
+
+* Port from `@crycode/eslint-config`
+
+## License
+
+MIT License
+
+Copyright (c) 2021-2023 Peter Müller <peter@crycode.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ eslint-config.js
@@ -0,0 +1,187 @@
+/**
+ * ESLint configuration to be used with plain JavaScript according to my
+ * personal preferences.
+ */
+
+module.exports = {
+
+  extends: 'eslint:recommended',
+
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'script',
+  },
+
+  plugins: [],
+
+  ignorePatterns: [
+    '/build/*',
+    '/dist/*',
+  ],
+
+  rules: {
+
+    'array-bracket-spacing': ['warn', 'always'],
+
+    'array-callback-return': 'warn',
+
+    'arrow-parens': ['warn', 'always'],
+
+    'arrow-spacing': 'warn',
+
+    'brace-style': [
+      'error',
+      '1tbs',
+      {
+        allowSingleLine: true,
+      },
+    ],
+
+    'comma-dangle': [
+      'warn',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+
+    'comma-spacing': 'error',
+
+    'default-param-last': 'error',
+
+    'dot-location': ['warn', 'property'],
+
+    'eol-last': 'warn',
+
+    'eqeqeq': 'warn',
+
+    'indent': [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        MemberExpression: 'off',
+      },
+    ],
+
+    'jsx-quotes': ['error', 'prefer-single'],
+
+    'keyword-spacing': 'error',
+
+    'linebreak-style': ['warn', 'unix'],
+
+    'lines-between-class-members': 'warn',
+
+    'camelcase': 'error',
+
+    'no-caller': 'error',
+
+    'no-console': 'error',
+
+    'no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true,
+      },
+    ],
+
+    'no-eval': 'error',
+
+    'no-implied-eval': 'error',
+
+    'no-invalid-this': 'error',
+
+    'no-label-var': 'warn',
+
+    'no-loop-func': 'error',
+
+    'no-loss-of-precision': 'error',
+
+    'no-mixed-operators': 'warn',
+
+    'no-redeclare': 'error',
+
+    'no-shadow': 'error',
+
+    'no-trailing-spaces': 'warn',
+
+    'no-undef-init': 'warn',
+
+    'no-unused-expressions': 'warn',
+
+    'no-var': 'error',
+
+    'object-curly-spacing': ['warn', 'always'],
+
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+
+    'quote-props': ['warn', 'as-needed'],
+
+    'quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+
+    'radix': 'warn',
+
+    'no-return-await': 'warn',
+
+    'semi': [
+      'warn',
+      'always',
+      {
+        omitLastInOneLineBlock: false,
+      },
+    ],
+
+    'semi-style': 'warn',
+
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+
+    'space-before-blocks': 'warn',
+
+    'space-before-function-paren': 'warn',
+
+    'space-infix-ops': [
+      'error',
+      {
+        int32Hint: false,
+      },
+    ],
+
+    'spaced-comment': [
+      'warn',
+      'always',
+      {
+        exceptions: ['-', '+', '*'],
+        line: {
+          markers: ['/'],
+        },
+        block: {
+          balanced: true,
+        },
+      },
+    ],
+
+    'yoda': 'warn',
+  },
+
+};
